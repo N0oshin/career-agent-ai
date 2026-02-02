d554b4fd0cf1221f6a16ddb2218ba77da5a37562
N0oshin <noosh1771@gmail.com>
2026-02-02 12:43:45 +0400

    updated the task description to get more relavant updates

--- main.py
+++ main.py
@@ -22,14 +22,18 @@ my_llm = LLM(
 career_agent = Agent(
     role="Job Application Assistant",
     goal="Identify if I have received any interview invites or important job updates or any new job postings.",
-    backstory="You are an expert career assistant. You are helping a student find a job. You are very careful and never miss an interview invitation or new job post. You only report on actual job-related emails, ignoring generic newsletters.",
+    backstory="You are an expert career assistant. You are helping a student find a job. You are very careful and never miss an interview invitation or new job post. You only report on actual job related emails, ignoring generic newsletters.",
     tools=[GmailTool.fetch_job_emails],
     llm=my_llm,
     verbose=True,  # This lets you see the agent "thinking" in the terminal
 )
 
 check_emails_task = Task(
-    description="Review all unread emails from todayfor mentions of 'interview' or 'job application' or any important job related updates, including application status changes or interview invites or new job postings or openings. If you find any, summarize the company name, the role, and whether I need to take action (like scheduling an interview).If no relevant emails are found, state that the inbox is clear for today.",
+    description="""1. Use the fetch_job_emails tool to get all emails from today.
+    2. Read through the subjects and snippets provided.
+    3. Filter out non-job related emails (newsletters, social media, etc.).
+    4. Focus only on: interview invites and job postings.
+    5. If the list is empty or no job emails exist, clearly state: 'Your inbox is clear of job updates for today.'""",
     expected_output="A clear, bulleted summary of job updates and urgent actions. withe name of company, role, and next steps if any including which company/organisation sent the email.",
     agent=career_agent,
 )
